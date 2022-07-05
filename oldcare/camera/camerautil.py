c6f406df236d76267e9d4e4f8967c7e8b80b08a4
Boching_Bot <19301060@bjtu.edu.cn>
2022-07-05 17:18:18 +0800

    '人脸情绪分析模型+推流到网页'

--- oldcare/camera/camerautil.py
+++ oldcare/camera/camerautil.py
@@ -2,6 +2,23 @@
 
 import cv2
 import threading
+from statistics import mode
+import cv2
+from keras.models import load_model
+import numpy as np
+from utils import preprocess_input
+
+# parameters for loading data and images
+detection_model_path = 'trained_models/facemodel/haarcascade_frontalface_default.xml'
+emotion_model_path = 'trained_models/float_models/fer2013_mini_XCEPTION.33-0.65.hdf5'
+emotion_labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy',
+                  4: 'sad', 5: 'surprise', 6: 'neutral'}
+
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+emotion_target_size = emotion_classifier.input_shape[1:3]
+emotion_window = []
+frame_window = 10
 
 
 class RecordingThread(threading.Thread):
@@ -32,6 +49,8 @@ class RecordingThread(threading.Thread):
 
 
 class VideoCamera(object):
+
+
     def __init__(self):
         # Open a camera
         self.cap = cv2.VideoCapture(0)
@@ -47,14 +66,48 @@ class VideoCamera(object):
         self.cap.release()
 
     def get_frame(self):
+
         ret, frame = self.cap.read()
 
         if ret:
             frame = cv2.flip(frame, 1)
+            #start
+            bgr_image = frame
+            gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+            rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+            faces = face_detection.detectMultiScale(gray_image, 1.3, 5)
+            for face_coordinates in faces:
+                x1, y1, width, height = face_coordinates
+                x1, y1, x2, y2 = x1, y1, x1 + width, y1 + height
+                # x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+                gray_face = gray_image[y1:y2, x1:x2]
+                try:
+                    gray_face = cv2.resize(gray_face, (emotion_target_size))
+                except:
+                    continue
+                gray_face = preprocess_input(gray_face, True)
+                gray_face = np.expand_dims(gray_face, 0)
+                gray_face = np.expand_dims(gray_face, -1)
+                emotion_prediction = emotion_classifier.predict(gray_face)
+                # emotion_probability = np.max(emotion_prediction)
+                emotion_label_arg = np.argmax(emotion_prediction)
+                emotion_text = emotion_labels[emotion_label_arg]
+                emotion_window.append(emotion_text)
+                if len(emotion_window) > frame_window:
+                    emotion_window.pop(0)
+                try:
+                    emotion_text = mode(emotion_window)
+                except:
+                    continue
+                color = (0, 0, 255)
+                cv2.rectangle(rgb_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.putText(rgb_image, emotion_text, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
+            bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+            frame = bgr_image
+
+
             ret, jpeg = cv2.imencode('.jpg', frame)
-
             return jpeg.tobytes()
-
         else:
             return None
 
